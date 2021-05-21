3b11e64ef0daf2e412d67d6da126b9a7d264284c
neeral1234 <76726903+neeral1234@users.noreply.github.com>
2021-05-21 12:21:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -33,17 +33,22 @@ function draw() {
     background(rgb(169,169,169));
     edges=createEdgeSprites();
     ball.bounceOff(edges);
+    music.play();
     //create edgeSprite
     if(block1.isTouching(ball)&&ball.bounceOff(block1)){
         ball.shapeColor="red";
     }
     if(block2.isTouching(ball)&&ball.bounceOff(block2)){
         ball.shapeColor="purple";
+        ball.velocityX=0;
+        ball.velocityY=0;
+        music.stop();
     }
     if(block3.isTouching(ball)&&ball.bounceOff(block3)){
         ball.shapeColor="blue";
     }
     if(block4.isTouching(ball)&&ball.bounceOff(block4)){
+        console.log("green");
         ball.shapeColor="green";
     }
     
